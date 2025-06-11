c71ffd827502ff97623c1a6b069d1a50436d44d9
Старцев Александр <a.startsev@reo.ru>
2025-06-11 23:41:31 +0800

    первый шаг: бутстрап, адаптив, о нас, моковые фото

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,41 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./home/home.component').then(c => c.HomeComponent)
+  },
+
+  {
+    path: 'about',
+    loadComponent: () => import('./about/about.component').then(c => c.AboutComponent)
+  },
+  {
+    path: 'programs',
+    loadComponent: () => import('./programs/programs.component').then(c => c.ProgramsComponent)
+  },
+  {
+    path: 'schedule',
+    loadComponent: () => import('./schedule/schedule.component').then(c => c.ScheduleComponent)
+  },
+  {
+    path: 'pricing',
+    loadComponent: () => import('./pricing/pricing.component').then(c => c.PricingComponent)
+  },
+  {
+    path: 'testimonials',
+    loadComponent: () => import('./testimonials/testimonials.component').then(c => c.TestimonialsComponent)
+  },
+  {
+    path: 'news',
+    loadComponent: () => import('./news/news.component').then(c => c.NewsComponent)
+  },
+  {
+    path: 'contact',
+    loadComponent: () => import('./contact/contact.component').then(c => c.ContactComponent)
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
