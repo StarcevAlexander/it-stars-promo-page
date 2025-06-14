04e899c5d36b39ac73eb3d8fca61e9ca126544a8
Старцев Александр <a.startsev@reo.ru>
2025-06-15 03:43:05 +0800

    карточка программы

--- src/app/programs/program-card/program-card.component.ts
+++ src/app/programs/program-card/program-card.component.ts
@@ -1,12 +1,25 @@
-import {Component, input} from '@angular/core';
+import {Component, inject, input, Signal} from '@angular/core';
+import {ProgramService} from "../program.service";
+import {LearningModule} from "../program.model";
+import {CommonModule} from '@angular/common';
+import {toSignal} from '@angular/core/rxjs-interop';
+import {map} from 'rxjs/operators';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-program-card',
-  standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './program-card.component.html',
-  styleUrl: './program-card.component.scss'
+  standalone: true,
+  styleUrls: ['./program-card.component.scss']
 })
-export class ProgramCardComponent  {
-  id = input.required<number>()
+export class ProgramCardComponent {
+  private programService = inject(ProgramService);
+  public id = input.required<string>();
+
+  // Находим модуль по ID
+  program: Signal<LearningModule | undefined> = toSignal(
+    this.programService.getLearningModules().pipe(
+      map(data => data.learningModules.find(module => module.id === +this.id()))
+    ))
 }
